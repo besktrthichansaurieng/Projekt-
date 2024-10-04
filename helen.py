09cc094efce61cbf82ba97b14a686051da8be06f
besktrthichansaurieng <trinhvtk.2408@gmail.com>
2024-10-04 20:54:12 +0700

    helen.py

--- helen.py
+++ helen.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1MTaUBDBwrzJbPwOya9yWSQiHEtvdBo9O
 """
 import subprocess
+subprocess.check_call(["pip", "install", "seaborn"])
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
