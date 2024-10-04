c8a2656cdfd92c6edb573a6568570d0ca32e9ae1
besktrthichansaurieng <trinhvtk.2408@gmail.com>
2024-10-04 18:58:17 +0700

    helen.py

--- helen.py
+++ helen.py
@@ -7,7 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/1MTaUBDBwrzJbPwOya9yWSQiHEtvdBo9O
 """
 
-pip install streamlit
+import subprocess
+subprocess.check_call(["pip", "install", "streamlit"])
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
